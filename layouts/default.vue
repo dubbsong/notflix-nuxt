c6e1e9d9e20b3a7e9556668c4dead248fe2834ce
dubbsong <dubbsong@gmail.com>
2020-06-22 14:44:09 +0900

    Set default style

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,3 +12,22 @@
     <!-- NavMobile -->
   </div>
 </template>
+
+<style lang="scss">
+#app {
+  #main {
+    min-height: calc(100vh - 140px);
+    background-color: #141414;
+    color: #fff;
+    margin-top: 68px;
+  }
+}
+
+@media (max-width: 768px) {
+  #app {
+    #main {
+      padding-bottom: 72px;
+    }
+  }
+}
+</style>
