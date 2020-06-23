ac358999a482d1a8af39d9cd5f2df78cbd76bbd3
dubbsong <dubbsong@gmail.com>
2020-06-23 15:34:35 +0900

    Add About page

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="about">
+    <!-- Title -->
+    <section class="section">
+      <div class="container">
+        <h3>About</h3>
+      </div>
+    </section>
+  </div>
+</template>
