bbf8dc9c40e5680d4642a2e14b3c3687a0d417d2
dubbsong <dubbsong@gmail.com>
2020-06-28 14:34:19 +0900

    Set test page

---
+++ pages/test.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="test">
+    <section class="section">
+      <h3 style="margin-bottom: 24px;">Test</h3>
+
+      <!-- JWT -->
+      <div class="card">
+        <div class="card-content">
+          <h4>JWT token</h4>
+
+          <b-button class="button is-dark" @click="checkJwt">
+            Check
+          </b-button>
+        </div>
+      </div>
+
+      <!-- Rate -->
+      <div class="card">
+        <div class="card-content">
+          <h4>Rate</h4>
+
+          <b-rate v-model="rate" icon-pack="mdi" icon="star"></b-rate>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rate: 2,
+  }),
+  methods: {
+    checkJwt() {
+      alert(sessionStorage.getItem('jwt'))
+    },
+  },
+  head: () => ({
+    title: 'Notflix Test',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Notflix Test',
+      },
+    ],
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+#test {
+  section {
+    .card {
+      margin-bottom: 24px;
+
+      .card-content {
+        h4 {
+          margin-bottom: 8px;
+        }
+      }
+    }
+  }
+}
+</style>
