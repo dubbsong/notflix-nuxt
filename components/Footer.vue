3a45b7283447de71580db6e6014ab963ae80b38b
dubbsong <dubbsong@gmail.com>
2020-06-28 14:12:50 +0900

    Modify Footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,75 +1,72 @@
 <template>
   <footer>
-    <div class="container">
-      <!-- Left -->
-      <div class="left">
-        <p>© 2020</p>
-        <a href="https://dubbsong.github.io" target="_blank">
-          ㅇㅁㅂ
-        </a>
-      </div>
-
-      <!-- Right -->
-      <ul class="right">
-        <li>
-          <a href="https://github.com/dubbsong" target="_blank">
-            <b-icon icon="github" />
-          </a>
-        </li>
-        <li>
-          <a href="https://dubbsong.github.io" target="_blank">
-            <img src="~/assets/img/omb.png" alt="omb img" width="20" />
-          </a>
-        </li>
-      </ul>
+    <!-- Left -->
+    <div class="left">
+      <p>© 2020</p>
+      <a href="https://dubbsong.github.io" target="_blank">
+        ㅇㅁㅂ
+      </a>
     </div>
+
+    <!-- Right -->
+    <ul class="right">
+      <!-- github -->
+      <li>
+        <a href="https://github.com/dubbsong" target="_blank">
+          <b-icon icon="github" />
+        </a>
+      </li>
+
+      <!-- omb -->
+      <li>
+        <a href="https://dubbsong.github.io" target="_blank">
+          <img src="~/assets/img/omb.png" alt="omb img" width="20" />
+        </a>
+      </li>
+    </ul>
   </footer>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 footer {
   background-color: #141414;
   color: #fff;
   display: flex;
+  justify-content: space-between;
   align-items: center;
+  height: 72px;
   padding: 0 5%;
 
-  .container {
+  /* Left */
+  .left {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    height: 72px;
 
-    /* Left */
-    .left {
-      display: flex;
-
-      p {
-        font-weight: bold;
-        margin-right: 4px;
-      }
-
-      a {
-        font-weight: bold;
-        color: #fff;
-      }
+    p {
+      font-weight: bold;
+      margin-right: 4px;
     }
 
-    /* Right */
-    .right {
-      display: flex;
+    a {
+      font-weight: bold;
+      color: #fff;
+    }
+  }
 
-      li {
+  /* Right */
+  .right {
+    display: flex;
+    align-items: center;
+
+    li {
+      margin-left: 8px;
+
+      a {
         display: flex;
-        margin-left: 8px;
+        align-items: center;
 
-        a {
-          display: flex;
-          align-items: center;
-
-          .icon {
-            color: #fff;
-          }
+        .icon {
+          color: #fff;
         }
       }
     }
