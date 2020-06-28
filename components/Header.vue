5c53956d3e54aa55a9def9d62e84f0a01fde8a32
dubbsong <dubbsong@gmail.com>
2020-06-28 14:07:20 +0900

    Modify Header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,96 +1,115 @@
 <template>
   <header>
-    <div class="container">
-      <!-- Left -->
-      <ul class="left">
-        <li>
-          <nuxt-link to="/">
-            <img src="~/assets/img/logo.png" alt="logo" width="100" />
-          </nuxt-link>
-        </li>
-        <li class="is-hidden-mobile">
-          <nuxt-link to="/about">
-            About
-          </nuxt-link>
-        </li>
-        <li class="is-hidden-mobile">
-          <nuxt-link to="/movies">
-            Movies
-          </nuxt-link>
-        </li>
-        <li class="is-hidden-mobile">
-          <nuxt-link to="/tv">
-            TV Shows
-          </nuxt-link>
-        </li>
-        <li class="is-hidden-mobile">
-          <nuxt-link to="/my-list">
-            My List
-          </nuxt-link>
-        </li>
-      </ul>
+    <!-- Left -->
+    <ul class="left">
+      <!-- logo -->
+      <li>
+        <nuxt-link to="/">
+          <img src="~/assets/img/logo.png" alt="logo" width="100" />
+        </nuxt-link>
+      </li>
 
-      <!-- Right -->
-      <ul class="right">
-        <li>
-          <nuxt-link to="/search">
-            <b-icon icon="magnify" />
-          </nuxt-link>
-        </li>
-        <li v-show="!$store.state.checkJwt">
-          <nuxt-link to="/login">
-            <b-icon icon="login-variant" />
-          </nuxt-link>
-        </li>
-        <li v-show="$store.state.checkJwt">
-          <b-dropdown position="is-bottom-left">
-            <a slot="trigger" role="button">
-              <b-icon icon="account-circle" />
+      <!-- about -->
+      <li class="is-hidden-mobile">
+        <nuxt-link to="/about">
+          About
+        </nuxt-link>
+      </li>
+
+      <!-- movies -->
+      <li class="is-hidden-mobile">
+        <nuxt-link to="/movies">
+          Movies
+        </nuxt-link>
+      </li>
+
+      <!-- tv shows -->
+      <li class="is-hidden-mobile">
+        <nuxt-link to="/tv">
+          TV Shows
+        </nuxt-link>
+      </li>
+
+      <!-- my list -->
+      <li class="is-hidden-mobile">
+        <nuxt-link to="/my-list">
+          My List
+        </nuxt-link>
+      </li>
+
+      <!-- i18n -->
+    </ul>
+
+    <!-- Right -->
+    <ul class="right">
+      <!-- search -->
+      <li>
+        <nuxt-link to="/search">
+          <b-icon icon="magnify" />
+        </nuxt-link>
+      </li>
+
+      <!-- login -->
+      <li v-show="!$store.state.checkJwt">
+        <nuxt-link to="/login">
+          <b-icon icon="login-variant" />
+        </nuxt-link>
+      </li>
+
+      <!-- account -->
+      <li v-show="$store.state.checkJwt">
+        <b-dropdown position="is-bottom-left">
+          <a slot="trigger" role="button">
+            <b-icon icon="account-circle" />
+          </a>
+
+          <!-- username -->
+          <b-dropdown-item has-link>
+            <nuxt-link to="/about">
+              <img src="~/assets/img/omb.png" alt="omb img" width="20" />
+              오늘도 많이 배웁니다.
+            </nuxt-link>
+          </b-dropdown-item>
+
+          <!-- hr -->
+          <hr class="dropdown-divider" />
+
+          <!-- about -->
+          <b-dropdown-item has-link>
+            <nuxt-link to="/about">
+              <b-icon icon="account" />
+              About
+            </nuxt-link>
+          </b-dropdown-item>
+
+          <!-- link -->
+          <b-dropdown-item has-link>
+            <a href="https://github.com/dubbsong/notflix-nuxt" target="_blank">
+              <b-icon icon="link-variant" />
+              Github repo
             </a>
+          </b-dropdown-item>
 
-            <b-dropdown-item has-link>
-              <nuxt-link to="/about">
-                <img src="~/assets/img/omb.png" alt="omb img" width="20" />
-                오늘도 많이 배웁니다.
-              </nuxt-link>
-            </b-dropdown-item>
-
-            <hr class="dropdown-divider" />
-
-            <b-dropdown-item has-link>
-              <nuxt-link to="/about">
-                <b-icon icon="account" />
-                About
-              </nuxt-link>
-            </b-dropdown-item>
-
-            <b-dropdown-item has-link>
-              <a
-                href="https://github.com/dubbsong/notflix-nuxt"
-                target="_blank"
-              >
-                <b-icon icon="link-variant" />
-                Github repo
-              </a>
-            </b-dropdown-item>
-
-            <b-dropdown-item has-link @click="handleLogout">
-              <a>
-                <b-icon icon="logout-variant" />
-                Logout
-              </a>
-            </b-dropdown-item>
-          </b-dropdown>
-        </li>
-      </ul>
-    </div>
+          <!-- logout -->
+          <b-dropdown-item has-link @click="handleLogout">
+            <a>
+              <b-icon icon="logout-variant" />
+              Logout
+            </a>
+          </b-dropdown-item>
+        </b-dropdown>
+      </li>
+    </ul>
   </header>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
-  mounted() {},
+  mounted() {
+    if (sessionStorage.getItem('jwt') !== null) {
+      this.$store.commit('checkJwt', true)
+    }
+  },
   methods: {
     handleLogout() {
       this.$store.dispatch('logout')
@@ -110,80 +129,105 @@ header {
   width: 100%;
   height: 68px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 0 5%;
   z-index: 10;
 
-  .container {
+  /* Left */
+  .left {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 100%;
 
-    /* Left */
-    .left {
+    li {
       display: flex;
+      align-items: center;
+      margin-right: 24px;
 
-      li {
+      a {
         display: flex;
         align-items: center;
-        margin-right: 24px;
+        font-size: 14px;
+        font-weight: bold;
+        color: #b3b3b3;
+        transition: all 0.2s ease-in-out;
 
-        a {
-          display: flex;
-          align-items: center;
-          font-size: 14px;
-          color: #696969;
-          transition: all 0.2s ease-in-out;
+        &.nuxt-link-exact-active {
+          color: #fff;
+        }
 
-          &.nuxt-link-exact-active {
-            color: #fff;
-          }
+        &:hover {
+          color: #fff;
+        }
+      }
 
-          &:hover {
-            color: #fff;
+      .dropdown {
+        .dropdown-menu {
+          .dropdown-content {
+            .dropdown-item {
+              color: #141414;
+            }
           }
         }
       }
     }
+  }
 
-    /* Right */
-    .right {
-      display: flex;
+  /* Right */
+  .right {
+    display: flex;
 
-      li {
+    li {
+      margin-left: 16px;
+
+      a {
         display: flex;
         align-items: center;
-        margin-left: 16px;
 
-        a {
-          .icon {
-            color: #fff;
-          }
+        .icon {
+          color: #fff;
         }
+      }
 
-        .dropdown {
-          .dropdown-menu {
-            .dropdown-content {
-              .has-link {
-                a {
-                  display: flex;
-                  align-items: center;
-                  font-weight: bold;
+      .dropdown {
+        .dropdown-menu {
+          .dropdown-content {
+            .has-link {
+              a {
+                display: flex;
+                align-items: center;
+                font-weight: bold;
 
-                  img {
-                    margin-right: 8px;
-                  }
+                img {
+                  margin-right: 8px;
+                }
 
-                  span {
-                    color: #141414;
-                    margin-right: 8px;
-                  }
+                span {
+                  color: #141414;
+                  margin-right: 8px;
                 }
               }
             }
           }
         }
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  header {
+    /* Left */
+    .left {
+      li {
+        align-items: flex-end;
+        margin-right: 8px;
+      }
+    }
+
+    /* Right */
+    .right {
+      li {
+        margin-left: 8px;
       }
     }
   }
