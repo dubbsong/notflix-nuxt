1cf344028ddc41a87f834aa4c879bd81f468184d
dubbsong <dubbsong@gmail.com>
2020-07-03 08:37:23 +0900

    Set search page with Axios

--- pages/search.vue
+++ pages/search.vue
@@ -9,19 +9,146 @@
           v-model="searchWord"
           type="search"
           icon="magnify"
-          placeholder="Search Movie or TV show"
+          placeholder="Search for Movie or TV show"
           icon-clickable
           @icon-click="handleSearch"
           @keyup.native.enter="handleSearch"
         ></b-input>
       </b-field>
-    </section>
 
-    <!-- Result -->
-    <section v-show="related !== ''" class="section">
-      <div class="result">
+      <div v-show="related !== ''" class="related">
         <h6>Explore titles related to:</h6>
         <h4>{{ related }}</h4>
+      </div>
+    </section>
+
+    <!-- Result Movies -->
+    <section v-show="related !== ''" class="section">
+      <!-- title -->
+      <h4>Movies</h4>
+
+      <!-- slider -->
+      <div v-if="resultMovies.length" class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in resultMovies"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
+
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
+
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.title.length > 16 ? i.title.slice(0, 16) + '...' : i.title
+                  }}
+                </h6>
+                <p v-if="i.release_date !== undefined">
+                  {{ i.release_date.slice(0, 4) }}
+                </p>
+                <p v-else>No information</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Result TV Shows -->
+    <section v-show="related !== ''" class="section">
+      <!-- title -->
+      <h4>TV Shows</h4>
+
+      <!-- slider -->
+      <div v-if="resultTVShows.length" class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in resultTVShows"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
+
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
+
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.name.length > 16 ? i.name.slice(0, 16) + '...' : i.name
+                  }}
+                </h6>
+                <p v-if="i.release_date !== undefined">
+                  {{ i.release_date.slice(0, 4) }}
+                </p>
+                <p v-else>No information</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 
@@ -35,6 +162,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import AlertModal from '~/components/AlertModal'
 
 export default {
@@ -44,6 +172,9 @@ export default {
   data: () => ({
     searchWord: '',
     related: '',
+    resultMovies: [],
+    resultTVShows: [],
+    isDisabled: true,
     alertModal: false,
     alertModalTitle: '',
   }),
@@ -55,8 +186,45 @@ export default {
         return
       }
 
+      const params = [
+        'api_key=b1aff257ceb0cbcdd236cef217694a61',
+        'language=en-US',
+      ].join('&')
+      const searchWord = this.searchWord
+
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?${params}&query=${searchWord}`
+        )
+        .then(({ data }) => {
+          this.resultMovies = data.results
+        })
+        .catch((error) => {
+          this.resultMovies = []
+          throw error
+        })
+
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/tv?${params}&query=${searchWord}`
+        )
+        .then(({ data }) => {
+          this.resultTVShows = data.results
+        })
+        .catch((error) => {
+          this.resultTVShows = []
+          throw error
+        })
+
       this.related = this.searchWord
       this.searchWord = ''
+    },
+    moveToDetail(val) {
+      if (val.title) {
+        this.$router.push(`/movie/${val.id}`)
+      } else {
+        this.$router.push(`/tv/${val.id}`)
+      }
     },
   },
   head: () => ({
@@ -76,16 +244,139 @@ export default {
 #search {
   section {
     h3 {
-      margin-bottom: 16px;
+      margin-bottom: 8px;
     }
 
-    .result {
+    .field {
+      margin-bottom: 0;
+    }
+
+    /* related */
+    .related {
       display: flex;
       align-items: center;
 
       h6 {
         font-weight: normal;
         margin-right: 8px;
+      }
+    }
+
+    /* slider */
+    .slider {
+      margin-top: -4px;
+      margin-left: -4px;
+
+      .slider-content {
+        display: flex;
+        overflow-x: auto;
+        padding: 16px 0;
+
+        &::-webkit-scrollbar {
+          display: none;
+        }
+
+        .slider-item {
+          position: relative;
+          min-width: 250px;
+          height: 140px;
+          margin: 0 4px;
+          transition: all 0.2s ease-in-out;
+          cursor: pointer;
+
+          &:hover {
+            transform: scale(1.2);
+            margin: 0 24px;
+          }
+
+          /* backdrop */
+          .backdrop {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+
+            img {
+              width: 100%;
+              height: 100%;
+            }
+          }
+
+          /* logo */
+          .logo {
+            img {
+              position: absolute;
+              top: 4px;
+              left: 2px;
+              width: 20px;
+            }
+          }
+
+          /* hovered */
+          .hovered {
+            position: absolute;
+            background-color: transparent;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-end;
+            padding: 4px 8px;
+            opacity: 0;
+            transition: all 0.2s ease-in-out;
+
+            &:hover {
+              background-color: rgba(0, 0, 0, 0.4);
+              opacity: 1;
+            }
+
+            /* left */
+            .left {
+              h6 {
+                font-size: 16px;
+              }
+
+              p {
+                font-size: 12px;
+                font-weight: bold;
+                margin-top: -4px;
+              }
+            }
+
+            /* right */
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  #search {
+    section {
+      padding: 1rem 1.5rem;
+
+      /* title */
+
+      /* slider */
+      .slider {
+        margin-top: 0;
+
+        .slider-content {
+          padding: 4px 0;
+          margin-left: -4px;
+
+          .slider-item {
+            /* backdrop */
+
+            /* logo */
+
+            /* hovered */
+            .hovered {
+              background-color: rgba(0, 0, 0, 0.2);
+              opacity: 1;
+            }
+          }
+        }
       }
     }
   }
