65e953de3e25a019397aecd2174d3e60fec3ce68
dubbsong <dubbsong@gmail.com>
2020-07-03 08:12:40 +0900

    Set Movies page with Axios

--- pages/movies.vue
+++ pages/movies.vue
@@ -11,17 +11,60 @@
       <h4>Popular</h4>
 
       <!-- slider -->
+      <div class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in popularData"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
 
-      <!-- data -->
-      <b-collapse :open="false">
-        <b-button slot="trigger" class="button is-light">
-          Data
-        </b-button>
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
 
-        <div class="content">
-          <p>{{ popularData }}</p>
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.title.length > 16 ? i.title.slice(0, 16) + '...' : i.title
+                  }}
+                </h6>
+                <p>{{ i.release_date.slice(0, 4) }}</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
         </div>
-      </b-collapse>
+      </div>
     </section>
 
     <!-- Now Playing -->
@@ -30,17 +73,122 @@
       <h4>Now Playing</h4>
 
       <!-- slider -->
+      <div class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in nowPlayingData"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
 
-      <!-- data -->
-      <b-collapse :open="false">
-        <b-button slot="trigger" class="button is-light">
-          Data
-        </b-button>
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
 
-        <div class="content">
-          <p>{{ nowPlayingData }}</p>
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.title.length > 16 ? i.title.slice(0, 16) + '...' : i.title
+                  }}
+                </h6>
+                <p>{{ i.release_date.slice(0, 4) }}</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
         </div>
-      </b-collapse>
+      </div>
+    </section>
+
+    <!-- Upcoming -->
+    <section class="section">
+      <!-- title -->
+      <h4>Upcoming</h4>
+
+      <!-- slider -->
+      <div class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in upcomingData"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
+
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
+
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.title.length > 16 ? i.title.slice(0, 16) + '...' : i.title
+                  }}
+                </h6>
+                <p>{{ i.release_date.slice(0, 4) }}</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
 
     <!-- Top Rated -->
@@ -49,17 +197,60 @@
       <h4>Top Rated</h4>
 
       <!-- slider -->
+      <div class="slider">
+        <div class="slider-content">
+          <div
+            v-for="(i, index) in topRatedData"
+            :key="index"
+            class="slider-item"
+            @click="moveToDetail(i)"
+          >
+            <!-- backdrop -->
+            <div class="backdrop">
+              <img
+                v-if="i.backdrop_path === null"
+                src="~/assets/img/no-poster.png"
+                alt="no poster"
+              />
+              <img
+                v-else
+                :src="'https://image.tmdb.org/t/p/w200' + i.backdrop_path"
+                alt="backdrop img"
+              />
+            </div>
 
-      <!-- data -->
-      <b-collapse :open="false">
-        <b-button slot="trigger" class="button is-light">
-          Data
-        </b-button>
+            <!-- logo -->
+            <div class="logo">
+              <img src="~/assets/img/logo-n.png" alt="logo n" />
+            </div>
 
-        <div class="content">
-          <p>{{ topRatedData }}</p>
+            <!-- hovered -->
+            <div class="hovered">
+              <!-- left -->
+              <div class="left">
+                <b-icon icon="play-circle" />
+                <h6>
+                  {{
+                    i.title.length > 16 ? i.title.slice(0, 16) + '...' : i.title
+                  }}
+                </h6>
+                <p>{{ i.release_date.slice(0, 4) }}</p>
+              </div>
+
+              <!-- right -->
+              <div class="right">
+                <b-rate
+                  v-model="i.vote_average"
+                  icon-pack="mdi"
+                  icon="star"
+                  :disabled="isDisabled"
+                  size="is-small"
+                ></b-rate>
+              </div>
+            </div>
+          </div>
         </div>
-      </b-collapse>
+      </div>
     </section>
   </div>
 </template>
@@ -71,11 +262,14 @@ export default {
   data: () => ({
     popularData: [],
     nowPlayingData: [],
+    upcomingData: [],
     topRatedData: [],
+    isDisabled: true,
   }),
   mounted() {
     this.getPopularData()
     this.getNowPlayingData()
+    this.getUpcomingData()
     this.getTopRatedData()
   },
   methods: {
@@ -113,6 +307,23 @@ export default {
           throw error
         })
     },
+    // Upcoming Data
+    getUpcomingData() {
+      const params = [
+        'api_key=b1aff257ceb0cbcdd236cef217694a61',
+        'language=en-US',
+      ].join('&')
+
+      axios
+        .get(`https://api.themoviedb.org/3/movie/upcoming?${params}`)
+        .then(({ data }) => {
+          this.upcomingData = data.results
+        })
+        .catch((error) => {
+          this.upcomingData = []
+          throw error
+        })
+    },
     // Top Rated Data
     getTopRatedData() {
       const params = [
@@ -130,6 +341,9 @@ export default {
           throw error
         })
     },
+    moveToDetail(val) {
+      this.$router.push(`/movie/${val.id}`)
+    },
   },
   head: () => ({
     title: 'Notflix Movies',
@@ -143,3 +357,129 @@ export default {
   }),
 }
 </script>
+
+<style lang="scss" scoped>
+#movies {
+  section {
+    /* title */
+
+    /* slider */
+    .slider {
+      margin-top: -4px;
+      margin-left: -4px;
+
+      .slider-content {
+        display: flex;
+        overflow-x: auto;
+        padding: 16px 0;
+
+        &::-webkit-scrollbar {
+          display: none;
+        }
+
+        .slider-item {
+          position: relative;
+          min-width: 250px;
+          height: 140px;
+          margin: 0 4px;
+          transition: all 0.2s ease-in-out;
+          cursor: pointer;
+
+          &:hover {
+            transform: scale(1.2);
+            margin: 0 24px;
+          }
+
+          /* backdrop */
+          .backdrop {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+
+            img {
+              width: 100%;
+              height: 100%;
+            }
+          }
+
+          /* logo */
+          .logo {
+            img {
+              position: absolute;
+              top: 4px;
+              left: 2px;
+              width: 20px;
+            }
+          }
+
+          /* hovered */
+          .hovered {
+            position: absolute;
+            background-color: transparent;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-end;
+            padding: 4px 8px;
+            opacity: 0;
+            transition: all 0.2s ease-in-out;
+
+            &:hover {
+              background-color: rgba(0, 0, 0, 0.4);
+              opacity: 1;
+            }
+
+            /* left */
+            .left {
+              h6 {
+                font-size: 16px;
+              }
+
+              p {
+                font-size: 12px;
+                font-weight: bold;
+                margin-top: -4px;
+              }
+            }
+
+            /* right */
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  #movies {
+    section {
+      padding: 1rem 1.5rem;
+
+      /* title */
+
+      /* slider */
+      .slider {
+        margin-top: 0;
+
+        .slider-content {
+          padding: 4px 0;
+          margin-left: -4px;
+
+          .slider-item {
+            /* backdrop */
+
+            /* logo */
+
+            /* hovered */
+            .hovered {
+              background-color: rgba(0, 0, 0, 0.2);
+              opacity: 1;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
